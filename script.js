3c7a67cb7c987e27ed12af382bb5535462c56c7f
Badr Moslih <moslihbadr2@gmail.com>
2023-05-21 19:49:41 +0100

    Implement snake body growth on food consumption

--- script.js
+++ script.js
@@ -1,36 +1,70 @@
 let score = document.getElementById('score');
 let highScore = document.getElementById('high-score');
 let gameBoard = document.querySelector('.gameBoard');
-let foodX ,foodY;
-let headX ,headY;
+let foodX, foodY;
+let headX, headY;
+let x = 0, y = 0;
+let snake = [];
 
-// change the food position randomly from 1 to 30
+// Change the food position randomly from 1 to 30
 function changeFoodPosition() {
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
 }
 
-// change the head position randomly from 1 to 30
+// Change the head position randomly from 1 to 30
 function changeHeadPosition() {
   headX = Math.floor(Math.random() * 30) + 1;
   headY = Math.floor(Math.random() * 30) + 1;
 }
 
-// draw the food
-function drawFood() {
-  gameBoard.innerHTML += `<span class="food" style="grid-area:${foodY} / ${foodX}"></span>`;
+// Change snake position
+function move() {
+  window.addEventListener('keydown', (e) => {
+    if (e.key === "ArrowUp") {
+      x = 0;
+      y = -1;
+    } else if (e.key === "ArrowDown") {
+      x = 0;
+      y = 1;
+    } else if (e.key === "ArrowRight") {
+      x = 1;
+      y = 0;
+    } else if (e.key === "ArrowLeft") {
+      x = -1;
+      y = 0;
+    }
+  });
 }
 
-// draw the sanke
-function drawSnake() {
-  gameBoard.innerHTML += `<span class="head" style="grid-area:${headY} / ${headX}"></span>`;
+// Draw the food and snake
+function drawGameBoard() {
+  let boardHTML = '';
+  boardHTML += `<span class="food" style="grid-area:${foodY} / ${foodX}"></span>`;
+  boardHTML += `<span class="head" style="grid-area:${headY} / ${headX}"></span>`;
+  gameBoard.innerHTML = boardHTML;
 }
 
+// Update the snake's position and draw the game board
+function updateGame() {
+  headX += x;
+  headY += y;
+  if (headX === foodX && headY === foodY) {
+    snake.push({
+      x:headX, y:headY
+    })
+    changeFoodPosition();
+  }
+  drawGameBoard();
+}
+
+// Initialize the game
 function init() {
-  drawFood();
-  drawSnake();
+  changeFoodPosition();
+  changeHeadPosition();
+  drawGameBoard();
+  setInterval(updateGame, 300);
+  move();
 }
 
-changeFoodPosition();
-changeHeadPosition();
 init();
